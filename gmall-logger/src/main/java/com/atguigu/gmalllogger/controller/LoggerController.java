6fb42530ba65deb50c91cce5865aa483bc6deccc
whalex <whalex@atguigu.com>
2020-08-14 17:27:15 +0800

    day01

---
+++ gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerController.java
@@ -0,0 +1,57 @@
+package com.atguigu.gmalllogger.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController  = @Controller+方法上的@ResponseBody
+@RestController
+@Slf4j
+public class LoggerController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @RequestMapping("t1")
+    public String test1() {
+        System.out.println("****************");
+        return "success";
+    }
+
+    @RequestMapping("t2")
+    public String test2(@RequestParam("name") String nn, @RequestParam("age") int age) {
+        System.out.println(nn + ":" + age);
+        return "success";
+    }
+
+    @RequestMapping("log")
+    public String getLogger(@RequestParam("logString") String logString) {
+
+        //添加时间戳
+        JSONObject jsonObject = JSON.parseObject(logString);
+        jsonObject.put("ts", System.currentTimeMillis());
+
+        //写入日志
+        log.info(jsonObject.toString());
+
+        //写入Kafka
+        if ("startup".equals(jsonObject.getString("type"))) {
+            //写入启动日志主题
+            kafkaTemplate.send("TOPIC_START", jsonObject.toString());
+        } else {
+            //写入事件日志主题
+            kafkaTemplate.send("TOPIC_EVENT", jsonObject.toString());
+        }
+
+        return "success";
+    }
+
+
+}
---
+++ gmall-mocker/src/main/java/com/atguigu/utils/RandomNum.java
@@ -0,0 +1,9 @@
+package com.atguigu.utils;
+
+import java.util.Random;
+
+public class RandomNum {
+    public static int getRandInt(int fromNum, int toNum) {
+        return fromNum + new Random().nextInt(toNum - fromNum + 1);
+    }
+}
